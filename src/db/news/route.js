2d8adb429e3e62a2a35ecb51c4b7395319cb30db
rashikbuksh <rashikbuksh@gmail.com>
2024-11-20 23:06:39 +0600

    Simplify route definitions by removing redundant validation for UUID parameters in news portal and documents entry routes

--- src/db/news/route.js
+++ src/db/news/route.js
@@ -10,14 +10,10 @@ const newsRouter = Router();
 
 // NOTE: news_portal routes
 newsRouter.get('/news-portal', newsOperations.selectAll);
-newsRouter.get('/news-portal/:uuid', validateUuidParam, newsOperations.select);
+newsRouter.get('/news-portal/:uuid', newsOperations.select);
 newsRouter.post('/news-portal', newsOperations.insert);
-newsRouter.put('/news-portal/:uuid', validateUuidParam, newsOperations.update);
-newsRouter.delete(
-	'/news-portal/:uuid',
-	validateUuidParam,
-	newsOperations.remove
-);
+newsRouter.put('/news-portal/:uuid', newsOperations.update);
+newsRouter.delete('/news-portal/:uuid', newsOperations.remove);
 newsRouter.get('/news-portal/latest-post', newsOperations.latestPost);
 newsRouter.get(
 	'/news-portal-details/by/:news_portal_uuid',
@@ -26,22 +22,10 @@ newsRouter.get(
 
 // NOTE: news_portal_entry routes
 newsRouter.get('/documents-entry', documentsEntryOperations.selectAll);
-newsRouter.get(
-	'/documents-entry/:uuid',
-	validateUuidParam,
-	documentsEntryOperations.select
-);
+newsRouter.get('/documents-entry/:uuid', documentsEntryOperations.select);
 newsRouter.post('/documents-entry', documentsEntryOperations.insert);
-newsRouter.put(
-	'/documents-entry/:uuid',
-	validateUuidParam,
-	documentsEntryOperations.update
-);
-newsRouter.delete(
-	'/documents-entry/:uuid',
-	validateUuidParam,
-	documentsEntryOperations.remove
-);
+newsRouter.put('/documents-entry/:uuid', documentsEntryOperations.update);
+newsRouter.delete('/documents-entry/:uuid', documentsEntryOperations.remove);
 newsRouter.get(
 	'/documents-entry/by/:news_portal_uuid',
 	documentsEntryOperations.selectByNewsPortalUuid
